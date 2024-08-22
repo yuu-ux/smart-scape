efe3a4e59f670ea19c0ef9bd32b3e633e5b6a61d
unknown <42-ehara@smartscape1.onmicrosoft.com>
2024-08-22 14:58:38 +0900

    feat: ヒストグラムの均一化とガウシアンブラーを適応

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 import copy
 import os
 
-# 画像読み込み・グレースケール
-# img_1 = cv2.imread('src/image1.png', 0)
-# img_2 = cv2.imread('src/image2.png', 0)
-img_3 = cv2.imread('src/image3.png', 0)
-img_4 = cv2.imread('src/image4.png', 0)
+# 画像読み込み
+img1 = cv2.imread('src/image3.png')
+img2 = cv2.imread('src/image4.png')
 
 # 画像のリサイズ
-height, width = img_3.shape
-img_4 = cv2.resize(img_4, (width, height))
+height, width = img1.shape[:2]
+img2 = cv2.resize(img2, (width, height))
+
+# グレースケール
+gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
 # ガンマ補正関数
 def adjust_gamma(image, gamma=1.0):
@@ -21,16 +23,30 @@ def adjust_gamma(image, gamma=1.0):
     return cv2.LUT(image, table)
 
 gamma = 1.5
-img3_gamma = adjust_gamma(img_3, gamma)
-img4_gamma = adjust_gamma(img_4, gamma)
+img1_gamma = adjust_gamma(gray1, gamma)
+img2_gamma = adjust_gamma(gray2, gamma)
+# plt.figure(figsize=(16, 5))
+# plt.subplot(1, 2, 1)
+# plt.imshow(img1_gamma, cmap='gray')
+# plt.subplot(1, 2, 2)
+# plt.imshow(img2_gamma, cmap='gray')
+# plt.show()
+
+img1_eq = cv2.equalizeHist(img1_gamma)
+img2_eq = cv2.equalizeHist(img2_gamma)
+# plt.figure(figsize=(16, 5))
+# plt.subplot(1, 2, 1)
+# plt.imshow(img1_gamma, cmap='gray')
+# plt.subplot(1, 2, 2)
+# plt.imshow(img2_gamma, cmap='gray')
+# plt.show()
 
 # 特徴点の検出
-# AKAZE特徴点検出器の初期化
 akaze = cv2.AKAZE_create()
 
 # 特徴点と記述子を検出
-kp1, des1 = akaze.detectAndCompute(img3_gamma, None)
-kp2, des2 = akaze.detectAndCompute(img4_gamma, None)
+kp1, des1 = akaze.detectAndCompute(img1_gamma, None)
+kp2, des2 = akaze.detectAndCompute(img2_gamma, None)
 
 # マッチャーの作成 (Brute Force Matcher)
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
@@ -39,6 +55,11 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 matches = bf.match(des1, des2)
 matches = sorted(matches, key=lambda x: x.distance)
 
+img_matches = cv2.drawMatches(img1_gamma, kp1, img2_gamma, kp2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+plt.figure(figsize=(16, 8))
+plt.imshow(img_matches)
+plt.show()
 # マッチング点の座標を抽出
 points1 = np.zeros((len(matches), 2), dtype=np.float32)
 points2 = np.zeros((len(matches), 2), dtype=np.float32)
@@ -51,19 +72,32 @@ for i, match in enumerate(matches):
 H, _ = cv2.findHomography(points2, points1, cv2.RANSAC)
 
 # 画像の変換
-transformed_img = cv2.warpPerspective(img4_gamma, H, (width, height))
+transformed_img = cv2.warpPerspective(img2_gamma, H, (width, height))
 
 # 画像を重ね合わせ
-combined_img = cv2.addWeighted(img3_gamma, 0.5, transformed_img, 0.5, 0)
+combined_img = cv2.addWeighted(img1_gamma, 0.7, transformed_img, 0.5, 0)
 
 # 結果をファイルに保存
 output_path = os.path.join('output', 'image.png')
 cv2.imwrite(output_path, combined_img)
 
-# 画像をカラーで読み込む (BGR形式)
+# 保存した画像を読み込む
 output_img = cv2.imread('output/image.png', 0)
 
-diff = cv2.absdiff(img_4, output_img)
+# グレースケールしたgray2と比較する
+diff = cv2.absdiff(output_img, gray2)
+
+# plt.figure(figsize=(16, 5))
+# plt.subplot(1, 3, 1)
+# plt.imshow(gray2, cmap='gray')
+# plt.subplot(1, 3, 2)
+# plt.imshow(output_img, cmap='gray')
+# plt.subplot(1, 3, 3)
+# plt.imshow(diff, cmap='gray')
+# plt.show()
+
+#  ガウシアンブラーの適応
+diff_blurred = cv2.GaussianBlur(diff, (5, 5), 0)
 
 # 初期値を設定
 threshold = 100
@@ -92,56 +126,3 @@ while True:
 
 # ウィンドウを閉じる
 cv2.destroyAllWindows()
-
-# # 2値化
-# # 適応的二値化（平均値）
-# # これがよさそうだけど、煮詰まったら確認の余地あり
-# res = cv2.adaptiveThreshold(diff, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-# masked_diff = cv2.bitwise_and(diff, res)
-# # diff = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
-# plt.imshow(masked_diff, cmap='gray')
-# plt.axis('off')
-# plt.show()
-
-# # ORB検出器を初期化
-# orb = cv2.ORB_create()
-
-# # 各画像の特徴点と記述子を検出
-# kp1, des1 = orb.detectAndCompute(img_1_gray, None)
-# kp2, des2 = orb.detectAndCompute(img_2_gray, None)
-
-# # 特徴点を画像に描画（画像1についてのみ）
-# img1_kp = cv2.drawKeypoints(img_1, kp1, None, color=(0, 255, 0), flags=0)
-
-# # 画像を引き算
-# img_diff = cv2.absdiff(img_1_gray, img_2_gray)
-
-# # 2値化
-# ret2, img_th = cv2.threshold(img_diff, 20, 255, cv2.THRESH_BINARY)
-
-# # 輪郭を検出
-# contours, hierarchy = cv2.findContours(img_th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# # 閾値以上の差分を四角で囲う
-# for i, cnt in enumerate(contours):
-#     x, y, width, height = cv2.boundingRect(cnt)
-#     if width > 20 or height > 20:
-#         cv2.rectangle(img1_kp, (x, y), (x + width, y + height), (0, 0, 255), 1)
-
-# # 画像表示の設定
-# plt.figure(figsize=(10, 3))  # ウィンドウサイズを設定
-# plt.subplots_adjust(wspace=0.05, hspace=0)  # サブプロット間の間隔を設定
-
-# # 元画像1を表示
-# plt.subplot(1, 3, 1)
-# plt.imshow(cv2.cvtColor(img_1, cv2.COLOR_BGR2RGB))
-
-# # 元画像2を表示
-# plt.subplot(1, 3, 2)
-# plt.imshow(cv2.cvtColor(img_2, cv2.COLOR_BGR2RGB))
-
-# # 比較画像を表示
-# plt.subplot(1, 3, 3)
-# plt.imshow(cv2.cvtColor(img1_kp, cv2.COLOR_BGR2RGB))
-
-# plt.show()  # 画像を表示
